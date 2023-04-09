b16f4672749523d27e823a344fc15248cfda63f5
Igor Vasilev <sonic-new78@yandex.ru>
2023-04-09 19:38:50 +0300

    Add main page

--- src/app/pages/main/main-page.component.ts
+++ src/app/pages/main/main-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'pt-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainPageComponent {
 
---
+++ src/app/pages/main/why/why.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WhyComponent } from './why.component';
+
+
+
+@NgModule({
+  declarations: [
+    WhyComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    WhyComponent
+  ]
+})
+export class WhyModule { }
